1b8ab3e48dd0985a9bfce91cecab5c1a7dfbee47
JesusMartin98A <74414655+JesusMartin98A@users.noreply.github.com>
2022-10-19 23:13:13 +0200

    Create reports.py

---
+++ reports.py
@@ -0,0 +1,15 @@
+# Generate a PDF file to send to the supplier indicating that the data was correctly processed using the ReportLab library.
+
+#!/usr/bin/env python3
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
+def generate_report(attachment, title, paragraph):
+    styles = getSampleStyleSheet()
+    report = SimpleDocTemplate(attachment)
+    report_title = Paragraph(title, styles["h1"])
+    report_info = Paragraph(paragraph, styles["BodyText"])
+    empty_line = Spacer(1,20)
+    report.build([report_title, empty_line, report_info])
